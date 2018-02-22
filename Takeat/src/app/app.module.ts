fb5124cb585b7c13906b2a7d10c9ea6dbe4856de
liorcaspi9 <liorcaspi9@gmail.com>
2018-02-22 18:36:23 +0200

    finished functionality of foodSelector component. still needs styling

--- Takeat/src/app/app.module.ts
+++ Takeat/src/app/app.module.ts
@@ -1,23 +1,29 @@
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+
 
 import {MatButtonModule} from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ServerAccessService } from './services/server-access/server-access.service';
+import { FoodSelectorComponent } from './pages/order-page/food-selector/food-selector.component';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    FoodSelectorComponent,
+    OrderPageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    HttpClientModule,
-    
+    MatCardModule
   ],
   providers: [ServerAccessService],
   bootstrap: [AppComponent]
