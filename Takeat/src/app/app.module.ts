d9997106dccbd3e008110c73c6da2ad1196b24a0
cindypomer <cindypomer@gmail.com>
2018-02-22 15:32:41 +0200

    intial

---
+++ Takeat/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
